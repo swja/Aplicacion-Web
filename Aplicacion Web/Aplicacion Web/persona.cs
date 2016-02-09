8ddf8ce58bb524854776aa0617ca8cd56b4fd6de
Santiago Jara <swja1991@hotmail.com>
2016-02-09 17:17:39 -0500

    Conexion exitosa

---
+++ Aplicacion Web/Aplicacion Web/persona.cs
@@ -0,0 +1,58 @@
+namespace Aplicacion_Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("proyecto1.persona")]
+    public partial class persona
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int id_persona { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int idTipo_Doc { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id_Tipo_Vehiculo { get; set; }
+
+        [StringLength(10)]
+        public string Cedula { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        [StringLength(10)]
+        public string Ced_pers_encontro { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nom_Pers_encontro { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        public string Ape_Pers_encontro { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Lugar_encontro { get; set; }
+
+        public DateTime Fecha_registro { get; set; }
+
+        public virtual tipo_vehiculo tipo_vehiculo { get; set; }
+
+        public virtual tipo_documento tipo_documento { get; set; }
+    }
+}
